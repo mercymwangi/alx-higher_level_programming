ab76a222a21d2d95cab0ec053f8ded2f15d52047
mercymwangi <mercien.m@gmail.com>
2023-08-12 13:25:13 -0700

    add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    """print the result of a and b."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
