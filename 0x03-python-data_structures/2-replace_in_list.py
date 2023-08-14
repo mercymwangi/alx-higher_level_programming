cfab45090ea88f79fc2201c8012b3555f1323927
mercymwangi <mercien.m@gmail.com>
2023-08-14 11:53:24 -0700

    task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#2-replace_in_list.py
+
+def replace_in_list(my_list, idx, element):
+    """function replaces an element of list at a specific position."""
+    
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+        return(my_list)
