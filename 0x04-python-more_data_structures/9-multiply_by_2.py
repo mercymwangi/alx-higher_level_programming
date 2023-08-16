064b694dad97737e8170e5d7d5cab5126615c6f5
mercymwangi <mercien.m@gmail.com>
2023-08-16 10:00:02 -0700

    task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    keys_list = list(new_dict.keys())
+
+    for j in keys_list:
+        new_dict[j] *= 2
+
+    return (new_dict)
+
